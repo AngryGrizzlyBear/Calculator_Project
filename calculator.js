eaa9a372bc7945435e8cdbf7363ea35eaccb5c28
Evan <24954397+AngryGrizzlyBear@users.noreply.github.com>
2024-08-06 09:35:40 -0700

    Added a function that takes 2 numbers and calls one of the functions created from earlier

--- calculator.js
+++ calculator.js
@@ -20,3 +20,35 @@ function divide(num1, num2) {
     return (num1 / num2)
 }
 
+function calculator(num1, op, num2) {
+    firstNumber = num1;
+    operator = op;
+    secondNumber = num2;
+
+    if (firstNumber !== null && secondNumber !== null) {
+        let result;
+
+        switch (operator) {
+            case '+':
+                result = add(firstNumber, secondNumber);
+                break;
+            case '-':
+                result = subtract(firstNumber, secondNumber);
+                break;
+            case '*':
+                result = multiply(firstNumber, secondNumber);
+                break;
+            case '/':
+                result = divide(firstNumber, secondNumber);
+                break;
+            default:
+                console.log('Error: Invalid operator');
+                break;
+        }
+        console.log(`${firstNumber} ${operator} ${secondNumber} = ${result}`);
+    } else {
+        console.error("Error: One or more variables are not set");
+    }
+}
+
+calculator(3, '/', 5)
